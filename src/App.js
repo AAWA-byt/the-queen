e9f4a297637f1db67c7a169ae3856519c546c561
AAWA-byt <mail@aaronwagner.earth>
2022-09-10 16:37:53 +0200

    :lipstick: :memo: added copyrigh under image design and updated css commentary

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div className='copyright'>
         <img src={the_queen} alt="the-queen" />
+        <p>Chris Levine, Lightness of Being (2004). Photo courtesy of Sotheby's London.</p>
+        </div>
           <p className='p-top'>Her Majesty</p>
             <h2>The Queen</h2>
               <p>1926 - 2022</p>

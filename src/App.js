c4953a19708fadc0df9bd3f08786be3816aa6cde
AAWA-byt <mail@aaronwagner.earth>
2022-09-10 18:55:05 +0200

    :sparkles: added announcement from the palace and logo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import the_queen from './assets/pictures/the-queen-closed-eyes.jpg';
+import the_palace from './assets/pictures/the-palace.png';
 import './App.css';
 
 function App() {
@@ -12,8 +13,19 @@ function App() {
           <p className='p-top'>Her Majesty</p>
             <h2>The Queen</h2>
               <p>1926 - 2022</p>
-
       </header>
+
+      <div className="App2-header">
+        <div className='App2-header__text'>
+          <center><img src={the_palace} alt="the-palace" /></center>
+          <p><i>"The Queen died peacefully at Balmoral this afternoon. The King and Queen Consort will remain at Balmoral this 
+                evening and will return to London tomorrow."</i></p>
+            <div className='App2-header__quote'>
+              <p>Thuesday, 8th September 2022 <br></br>
+                 Announcement from Buckingham Palace</p>
+            </div>      
+        </div>
+      </div>
     </div>
   );
 }
